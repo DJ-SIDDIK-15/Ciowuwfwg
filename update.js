5365c62ae96c1b35aebb819c8d4d87bae842f7c9
DJ-SIDDIK-15 <siddikkhan79321580@gmail.com>
2025-06-14 09:05:46 +0600

    Update update.js

--- update.js
+++ update.js
@@ -22,8 +22,8 @@ const logger = createGradientLogger();
 
 
 
-const REPO_OWNER = 'samirxpikachuio';
-const REPO_NAME = 'XaR-V2';
+const REPO_OWNER = 'DJ-SIDDIK-15';
+const REPO_NAME = 'XaRs-V2';
 
 const VERSION_FILE = path.join(__dirname, 'version.txt');
 
